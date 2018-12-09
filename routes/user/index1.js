4a1032f2f30ec9884936d94a1b2c29af15d5360f
nnht <951982555@qq.com>
2018-12-09 19:45:45 +0800

    修改

--- routes/user/index1.js
+++ routes/user/index1.js
@@ -1,11 +1,29 @@
 var query = require('./query');
 var fn = function(req, res, next) {
-    query('select * from shop', function(err, result) {
+    var pagenum = req.query.pagenum;
+    var num = req.query.num;
+    var total = 0;
+    query('select count(*) from shop', function(err, result) {
         if (err) {
             return res.json({ code: 0, msg: err });
         }
-        res.json({ code: 1, data: result });
+        total = Math.ceil((result[0]['count(*)']) / num);
+        // res.json({ code: 1, data: result });
+        queryUserlist(total);
     })
+
+    function queryUserlist() {
+        var start = (pagenum - 1) * num;
+        var sqlStr = `select * from shop order by id limit ${start},${num}`;
+        query(sqlStr, function(err, result) {
+            if (err) {
+                res.json({ code: 0, msg: err });
+            } else {
+                // console.log(result);
+                res.json({ code: 1, data: result, total: total });
+            }
+        })
+    }
 }
 
 module.exports = {
